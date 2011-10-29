fde90b74b2a2055cc2ea9b890adf28729bdddf39
artem.gevorkyan <artem.gevorkyan@9bd5d3b4-246e-e46f-6818-ebf78c9435f2>
2011-10-29 12:22:04 +0000

    - skeleton of easy-addin project.

---
+++ trunk/tests/EasyAddinTests.cpp
@@ -0,0 +1,21 @@
+#include <easy-addin/addin.h>
+
+//using namespace std;
+using namespace System;
+using namespace Microsoft::VisualStudio::TestTools::UnitTesting;
+
+namespace ea
+{
+	namespace tests
+	{
+		[TestClass]
+		public ref class EasyAddinTests
+		{
+		public:
+			[TestMethod]
+			void Test1()
+			{
+			}
+		};
+	}
+}
