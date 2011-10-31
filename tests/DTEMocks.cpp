22d0aa33578c9968d5069ff110ae0b34fe33d456
artem.gevorkyan <artem.gevorkyan@9bd5d3b4-246e-e46f-6818-ebf78c9435f2>
2011-10-31 10:02:30 +0000

    - command addition cycle completed.

--- tests/DTEMocks.cpp
+++ tests/DTEMocks.cpp
@@ -7,11 +7,15 @@ namespace ea
 	namespace tests
 	{
 		DTEMock::DTEMock()
+			: command_bars(new mock_com_object<IDispatch>())
 		{	}
 
 		DTEMock::DTEMock(bool &released_flag)
-			: base_type(released_flag)
+			: base_type(released_flag), command_bars(new mock_com_object<IDispatch>())
 		{	}
+
+		STDMETHODIMP DTEMock::get_CommandBars(IDispatch **ppcbs)
+		{	return command_bars->QueryInterface(ppcbs);	}
 
 		STDMETHODIMP DTEMock::get_Commands(msaddin::Commands **ppCommands)
 		{	return static_cast<IUnknown *>((new CommandsMock(commands_list)))->QueryInterface(ppCommands);	}
@@ -21,6 +25,7 @@ namespace ea
 			: _commands_list(&commands_list)
 		{	}
 
+		
 		STDMETHODIMP CommandsMock::raw_AddNamedCommand(msaddin::AddIn *AddInInstance, BSTR Name, BSTR ButtonText, BSTR Tooltip, VARIANT_BOOL MSOButton, long Bitmap, SAFEARRAY **ContextUIGUIDs, long vsCommandDisabledFlagsValue, msaddin::Command **pVal)
 		{
 			Assert::IsTrue(::SysStringLen(Name) == wcslen(Name));
@@ -30,12 +35,19 @@ namespace ea
 			Assert::IsTrue(0 == ContextUIGUIDs);
 			Assert::IsTrue(16 == vsCommandDisabledFlagsValue);
 			
-			DTEMock::command c = {	AddInInstance, Name, ButtonText, Tooltip	};
+			msaddin::CommandPtr created_command(new CommandMock);
+
+			DTEMock::command c = {	AddInInstance, Name, ButtonText, Tooltip, created_command	};
 
 			_commands_list->push_back(c);
+			created_command->QueryInterface(pVal);
 
 			return S_OK;
 		}
+
+
+		CommandMock::CommandMock()
+		{	}
 
 
 		AddInMock::AddInMock()
